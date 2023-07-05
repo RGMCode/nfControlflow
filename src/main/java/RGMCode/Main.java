c5e0d908681fe9bed47583190b56173c57466fc7
mrg <rgmengedoth@gmail.com>
2023-07-05 15:00:23 +0200

    switchcase + if

--- src/main/java/RGMCode/Main.java
+++ src/main/java/RGMCode/Main.java
@@ -5,12 +5,37 @@ public class Main {
         System.out.println("Hello world!");
     }
 
-    public static String checkAnzahl(int num1){
+    public static String checkAnzahlSimple(int num1){
         if (num1 < 30){
             return "Maximale Personenzahl nicht überschritten";
         } else {
             return "Zu viele Personen";
         }
     }
+
+
+
+    public static String checkAnzahl(int num1, String alarmstufe){
+        switch (alarmstufe.toLowerCase()) {
+            case "rot":
+                return "Keine Personen erlaubt";
+            case "gelb":
+                if (num1 <= 30) {
+                    return "Alarmstufe grün Maximale Personenzahl nicht überschritten";
+                } else {
+                    return "Zu viele Personen";
+                }
+            case "grün":
+                if (num1 <= 60) {
+                    return "Alarmstufe gelb Maximale Personenzahl nicht überschritten";
+                } else {
+                    return "Zu viele Personen";
+                }
+            default:
+                return "Ungültige Alarmstufe";
+        }
+    }
+
+
 
 }
--- src/test/java/RGMCode/MainTest.java
+++ src/test/java/RGMCode/MainTest.java
@@ -9,7 +9,7 @@ class MainTest {
     @Test
     void alarmTestUnter30(){
         int num1 = 29;
-        String actual = Main.checkAnzahl(num1);
+        String actual = Main.checkAnzahlSimple(num1);
         assertEquals("Maximale Personenzahl nicht überschritten", actual);
         //System.out.println("Die Anzahl der Personen ist: " + num1);
     }
@@ -17,9 +17,53 @@ class MainTest {
     @Test
     void alarmTestUeber30(){
         int num1 = 30;
-        String actual = Main.checkAnzahl(num1);
+        String actual = Main.checkAnzahlSimple(num1);
         assertEquals("Zu viele Personen", actual);
         //System.out.println("Die Anzahl der Personen ist: " + num1);
     }
 
+    /* ********************************************** */
+
+    @Test
+    void alarmTestRot() {
+        int num1 = 0;
+        String alarmstufe = "rot";
+        String actual = Main.checkAnzahl(num1, alarmstufe);
+        assertEquals("Keine Personen erlaubt", actual);
+    }
+
+    @Test
+    void alarmTestGelbUnter30() {
+        int num1 = 29;
+        String alarmstufe = "gelb";
+        String actual = Main.checkAnzahl(num1, alarmstufe);
+        assertEquals("Alarmstufe grün Maximale Personenzahl nicht überschritten", actual);
+    }
+
+    @Test
+    void alarmTestGelbUeber30() {
+        int num1 = 30;
+        String alarmstufe = "gelb";
+        String actual = Main.checkAnzahl(num1, alarmstufe);
+        assertEquals("Alarmstufe grün Maximale Personenzahl nicht überschritten", actual);
+    }
+
+    @Test
+    void alarmTestGruenUnter60() {
+        int num1 = 59;
+        String alarmstufe = "grün";
+        String actual = Main.checkAnzahl(num1, alarmstufe);
+        assertEquals("Alarmstufe gelb Maximale Personenzahl nicht überschritten", actual);
+    }
+
+    @Test
+    void alarmTestGruenUeber60() {
+        int num1 = 61;
+        String alarmstufe = "rot";
+        String actual = Main.checkAnzahl(num1, alarmstufe);
+        assertEquals("Keine Personen erlaubt", actual);
+    }
+
+
 }
+
